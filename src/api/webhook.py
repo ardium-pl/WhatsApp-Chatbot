fac155ff5febae45fcf3b1d823d6ba7de778f6db
Szymon <szymon.skowron@ardium.pl>
2024-09-03 22:03:08 +0200

    Minor try/catch block change

--- src/api/webhook.py
+++ src/api/webhook.py
@@ -11,56 +11,53 @@ rag_engine = RAGEngine()
 
 @webhook_bp.route('/webhook', methods=['POST'])
 def webhook():
+    if not request.is_json:
+        print("❌ Invalid request data type. Expected data type is JSON.")
+        return '❌', 400
+
     try:
         data = request.get_json()
+        main_request_body = data['entry'][0]['changes'][0]['value']
 
-        try:
-            main_request_body = data['entry'][0]['changes'][0]['value']
+        errors = main_request_body.get('errors')
+        statuses = main_request_body.get('statuses')
+        messages = main_request_body.get('messages')
 
-            errors = main_request_body.get('errors')
-            statuses = main_request_body.get('statuses')
-            messages = main_request_body.get('messages')
+        if errors:
+            print(f"⚙️ Request contained an errors field:"
+                    f"\tErrors: {errors}")
+        if statuses:
+            print(f'⚙️ Message status: {statuses[0].get("status")}')
+        if messages:
+            incoming_message = messages[0]
+            sender_phone_number = incoming_message.get("from")
 
-            if errors:
-                print(f"⚙️ Request contained an errors field:"
-                      f"\tErrors: {errors}")
-            if statuses:
-                print(f'⚙️ Message status: {statuses[0].get("status")}')
-            if messages:
-                incoming_message = messages[0]
-                sender_phone_number = incoming_message.get("from")
+            # Check if the incoming message contains text
+            if incoming_message.get('type') == 'text':
+                user_query = incoming_message['text'].get('body')
 
-                # Check if the incoming message contains text
-                if incoming_message.get('type') == 'text':
-                    user_query = incoming_message['text'].get('body')
+                print(f'✅ Received a POST request containing a text message:\n'
+                        f'\t📩 Message text: {user_query}\n'
+                        f'\t📞 Sender phone number: {sender_phone_number}')
 
-                    print(f'✅ Received a POST request containing a text message:\n'
-                          f'\t📩 Message text: {user_query}\n'
-                          f'\t📞 Sender phone number: {sender_phone_number}')
+                # Respond with AI answer
+                ai_answer = rag_engine.process_query(user_query)
+                WhatsAppClient.send_message(ai_answer, sender_phone_number)
+                whatsapp_logger.info('AI answer sent successfully')
 
-                    # Respond with AI answer
-                    ai_answer = rag_engine.process_query(user_query)
-                    WhatsAppClient.send_message(ai_answer, sender_phone_number)
-                    whatsapp_logger.info('AI answer sent successfully')
+                # Insert the query-answer pair into MySQL database
+                insert_data_mysql(sender_phone_number, user_query, ai_answer)
+                # mysql_logger.info('Query-answer pair inserted into MySQL')
 
-                    # Insert the query-answer pair into MySQL database
-                    insert_data_mysql(sender_phone_number, user_query, ai_answer)
-                    # mysql_logger.info('Query-answer pair inserted into MySQL')
-
-                else:
-                    print(f"⚙️ Received POST request doesn't contain text.\n"
-                          f'\t📩 Message type: {incoming_message.get("type")}.')
-
-        except Exception as e:
-            print(f"❌ Error accessing a main request body.\n"
-                  f"\tError message: {e}")
-        finally:
-            return '✅', 200
+            else:
+                print(f"⚙️ Received POST request doesn't contain text.\n"
+                        f'\t📩 Message type: {incoming_message.get("type")}.')
 
     except Exception as e:
-        print(f'❌ An error occurred during processing the request.\n'
-              f'\tError messages {e}.')
-        return '❌', 400
+        print(f"❌ Error accessing a main request body.\n"
+                f"\tError message: {e}")
+    finally:
+        return '✅', 200
 
 
 @webhook_bp.route('/webhook', methods=['GET'])
