a71195c8bc5b57a818793e1fb52694122e3b0769
Szymon Florek <szymon.florek@student.wat.edu.pl>
2024-09-07 19:16:30 +0200

    Remove mysql-connector-python and aiomysql dependencies, update system prompt in prepare_messages function

--- src/database/mysql_queries.py
+++ src/database/mysql_queries.py
@@ -5,7 +5,6 @@ from src.config import MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL
 
 async def insert_data_mysql(sender_phone_number, user_query, ai_answer):
     try:
-        # Używamy asyncmy.create_pool do utworzenia puli połączeń
         async with asyncmy.create_pool(
                 host=MYSQL_HOST,
                 port=int(MYSQL_PORT),
@@ -15,37 +14,27 @@ async def insert_data_mysql(sender_phone_number, user_query, ai_answer):
         ) as pool:
             async with pool.acquire() as conn:
                 async with conn.cursor() as cur:
-
-                    # Konwersja numeru telefonu do bigint
                     try:
                         whatsapp_number_id = int(sender_phone_number)
                     except ValueError:
                         mysql_logger.error(f"❌ Invalid phone number format: {sender_phone_number}")
                         return
 
-                    # Pobieranie user_id na podstawie numeru telefonu
                     await cur.execute("SELECT id FROM users WHERE whatsapp_number_id = %s", (whatsapp_number_id,))
                     result = await cur.fetchone()
 
-                    # Wprowadzenie numeru użytownika do tabeli users, jesli jeszcze go tam nie
                     if not result:
                         mysql_logger.info("🔧 Message was sent by the unknown user. Adding new user to the database...")
                         await cur.execute("INSERT INTO users (whatsapp_number_id) VALUES (%s)", (whatsapp_number_id,))
                         await conn.commit()
                         mysql_logger.info("✅ New user added!")
 
-                        # Retrieve the ID of the inserted row - VERSION 1
-                        new_user_id = await cur.lastrowid
-                        mysql_logger.info(f"➡️ ID retrieved by the cur.lastrowid method: {new_user_id}.")
-
-                        # Retrieve the ID of the inserted row - VERSION 2
+                        # Retrieve the ID of the inserted row
                         await cur.execute("SELECT id FROM users WHERE whatsapp_number_id = %s", (whatsapp_number_id,))
                         result = await cur.fetchone()
-                        mysql_logger.info(f"➡️ ID retrieved by the standard SELECT query: {result[0]}.")
 
-                    # Wstawianie danych zapytania i odpowiedzi użytkownika
                     if result:
-                        user_id = result[0]  # user_id jest intem
+                        user_id = result[0]
                         await cur.execute("INSERT INTO queries (user_id, query, answer) VALUES (%s, %s, %s)",
                                           (user_id, user_query, ai_answer))
                         await conn.commit()
@@ -54,7 +43,6 @@ async def insert_data_mysql(sender_phone_number, user_query, ai_answer):
                         mysql_logger.warning(f"❌ No matching user found for WhatsApp number ID: {whatsapp_number_id}")
     except Exception as e:
         mysql_logger.error(f"❌ An error occurred during MySQL operation: {e}")
-
 
 async def get_recent_queries(sender_phone_number):
     try:
