52104f6f85d0ed9eabf7534df822f5e496c40765
Szymon Florek <szymon.florek@student.wat.edu.pl>
2024-09-07 20:50:17 +0200

    Add error handling for API requests in user service

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from src.api.webhook import webhook_bp
 from src.config import PORT
 from src.worker import Worker, run_worker
 from src.logger import main_logger
-from src.database.mysql_queries import initialize_pools, close_pools
 
 app = Flask(__name__)
 app.register_blueprint(webhook_bp)
@@ -26,25 +25,11 @@ async def run_flask():
     await serve(app, config)
 
 
-async def initialize_app():
-    main_logger.info("🔥Initializing MySQL connection pools")
-    await initialize_pools()
-
-
-async def shutdown_app():
-    main_logger.info(" 👋 Closing MySQL connection pools")
-    await close_pools()
-
-
 async def main():
     main_logger.info("Starting the application")
-    await initialize_app()
-    try:
-        flask_task = asyncio.create_task(run_flask())
-        worker_task = asyncio.create_task(worker.run())
-        await asyncio.gather(flask_task, worker_task)
-    finally:
-        await shutdown_app()
+    flask_task = asyncio.create_task(run_flask())
+    worker_task = asyncio.create_task(worker.run())
+    await asyncio.gather(flask_task, worker_task)
 
 
 if __name__ == '__main__':
