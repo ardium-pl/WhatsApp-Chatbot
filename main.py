534f2259d6900ee3f26e596191e48d0acea3cb90
Szymon Florek <szymon.florek@student.wat.edu.pl>
2024-09-12 18:03:00 +0200

    Refactor to support multiple MySQL connection pools and add TODO comments for error handling and message type handling

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from hypercorn.asyncio import serve
 from src.api.webhook import webhook_bp
 from src.config import PORT
 from src.logger import main_logger
-from src.database.mysql_queries import initialize_connection_pool, close_connection_pool
+from src.database.mysql_queries import initialize_connection_pools, close_connection_pools
 
 app = Quart(__name__)
 app.register_blueprint(webhook_bp)
@@ -13,12 +13,12 @@ app.register_blueprint(webhook_bp)
 
 @app.before_serving
 async def before_serving():
-    await initialize_connection_pool()
+    await initialize_connection_pools()
 
 
 @app.after_serving
 async def after_serving():
-    await close_connection_pool()
+    await close_connection_pools()
 
 
 async def run_quart():
